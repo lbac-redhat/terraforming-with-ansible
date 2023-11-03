eafc0f6bb5381b6f0705c8ff61abbd3158dae579
Luka Bac <lbac@redhat.com>
2023-11-03 15:34:21 +0100

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.7.4"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+locals {
+  machines = {
+
+    "machine1" = {
+      "vCPUs"  = 2,
+      "memory" = 1024
+    },
+
+    "machine2" = {
+      "vCPUs"  = 1,
+      "memory" = 512
+    }
+
+  }
+}
+
+resource "libvirt_volume" "tf_volumes" {
+  for_each = local.machines
+
+  name             = each.key
+  base_volume_name = "rhel9.2-base.qcow2"
+}
+
+resource "libvirt_domain" "tf_vms" {
+  for_each = local.machines
+
+  name = each.key
+  cpu {
+    mode = "host-passthrough"
+  }
+  description = "Created by Terraform"
+  vcpu        = each.value.vCPUs
+  memory      = each.value.memory
+  disk {
+    volume_id = libvirt_volume.tf_volumes[each.key].id
+  }
+}
